6d1862e4387927fba4a6dab48f5ea653e8acf199
Stepan Goremykin <s.goremykin@paralect.com>
2020-04-03 13:21:37 +0300

    Fixed google authorization

--- src/app/Api/Core/DAL/Documents/User/User.cs
+++ src/app/Api/Core/DAL/Documents/User/User.cs
@@ -8,18 +8,20 @@ namespace Api.Core.DAL.Documents.User
         public User()
         {
             OAuth = new OAuthSettings();
-            ResetPasswordToken = string.Empty;
         }
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [BsonIgnoreIfNull]
         public string PasswordHash { get; set; }
         public string Email { get; set; }
         public bool IsEmailVerified { get; set; }
+        [BsonIgnoreIfNull]
         public string SignupToken { get; set; }
         [BsonElement("oauth")]
         public OAuthSettings OAuth { get; set; }
         public DateTime LastRequest { get; set; }
+        [BsonIgnoreIfNull]
         public string ResetPasswordToken { get; set; }
         
         public class OAuthSettings
