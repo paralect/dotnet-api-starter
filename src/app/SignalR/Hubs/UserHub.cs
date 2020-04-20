48e709ac24d19a43ac785ddc60bde3238cc0860f
Stepan Goremykin <s.goremykin@paralect.com>
2020-04-20 12:23:57 +0300

    Added signalr server

---
+++ src/app/SignalR/Hubs/UserHub.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class UserHub : Hub
+    {
+        public async Task Subscribe(string roomId)
+        {
+            if (HasAccessToRoom(roomId))
+            {
+
+            }
+        }
+
+        private bool HasAccessToRoom(string roomId)
+        {
+            var roomData = roomId.Split('-');
+            var roomType = roomData[0];
+            var id = roomData[1];
+
+            return Context.User.Identity.Name == id;
+        }
+    }
+}
---
+++ src/app/SignalR/Startup.cs
@@ -0,0 +1,87 @@
+using Common.DAL;
+using Common.DAL.Interfaces;
+using Common.DAL.Repositories;
+using Common.Middleware;
+using Common.Services;
+using Common.Services.Interfaces;
+using Common.Settings;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SignalR.Hubs;
+
+namespace SignalR
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureDi(services);
+            ConfigureDb(services);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", builder =>
+                {
+                    builder
+                        .WithOrigins("http://localhost:3002") // TODO: replace with configuration
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("AllowSpecificOrigin");
+            app.UseRouting();
+            app.UseTokenAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<UserHub>("");
+            });
+        }
+
+        private void ConfigureDi(IServiceCollection services)
+        {
+            services.AddTransient<ITokenService, TokenService>();
+            services.AddTransient<ITokenRepository, TokenRepository>();
+
+            services.AddTransient<IDbContext, DbContext>();
+            services.AddTransient<IIdGenerator, IdGenerator>();
+
+            services.Configure<DbSettings>(options => { _configuration.GetSection("MongoConnection").Bind(options); });
+            services.Configure<TokenExpirationSettings>(options => { _configuration.GetSection("TokenExpiration").Bind(options); });
+        }
+
+        private void ConfigureDb(IServiceCollection services)
+        {
+            var dbSettings = new DbSettings();
+            _configuration.GetSection("MongoConnection").Bind(dbSettings);
+
+            services.InitializeDb(dbSettings);
+        }
+    }
+}
