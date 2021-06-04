82f7b075329a5a1f728c0fd0d093ea1e71552be0
Ilya Hryapko <i.hryapko@paralect.com>
2021-06-04 20:29:23 +0300

    added constraint on non-numeric field and increment value

--- src/app/Common/DAL/UpdateDocumentOperators/IncrementOperator.cs
+++ src/app/Common/DAL/UpdateDocumentOperators/IncrementOperator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using Common.DAL.Documents;
 using MongoDB.Driver;
@@ -7,11 +9,29 @@ namespace Common.DAL.UpdateDocumentOperators
 {
     public class IncrementOperator<TDocument, TItem>: IUpdateOperator<TDocument> where TDocument: BaseDocument
     {
+        private static readonly HashSet<Type> _allowedTypes;
         private readonly Expression<Func<TDocument, TItem>> _field;
         private readonly TItem _incrementByValue;
 
+        static IncrementOperator()
+        {
+            var types = new[]
+            {
+                typeof(byte), typeof(sbyte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long),
+                typeof(ulong), typeof(float), typeof(double), typeof(decimal),
+            };
+            
+            _allowedTypes =
+                new HashSet<Type>(types.Concat(types.Select(t => typeof(Nullable<>).MakeGenericType(t))));
+        }
+
         public IncrementOperator(Expression<Func<TDocument, TItem>> field, TItem incrementByValue)
         {
+            if (!_allowedTypes.Contains(field.ReturnType) || !_allowedTypes.Contains(incrementByValue.GetType()))
+            {
+                throw new ArgumentException($"Only {string.Join(", ", _allowedTypes)} types is allowed");
+            }
+            
             _field = field;
             _incrementByValue = incrementByValue;
         }
