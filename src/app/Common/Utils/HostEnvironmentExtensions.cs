ed99fa5e51945de1552ce08af243215408ac0aba
Alexander Yarmolovich <yarmolovichalex@paralect.com>
2022-04-01 12:02:32 +0200

    configure logging for Api.NoSql

---
+++ src/app/Common/Utils/HostEnvironmentExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+namespace Common.Utils;
+
+public static class HostEnvironmentExtensions
+{
+    public static Logger BuildLogger(this IHostEnvironment hostEnvironment) =>
+        new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .Filter.ByExcluding("RequestPath = '/health' and StatusCode = 200")
+            .WriteTo.Logger(lc =>
+            {
+                if (hostEnvironment.IsDevelopment())
+                {
+                    lc.WriteTo.Console();
+                }
+                else
+                {
+                    lc.WriteTo.Console(new RenderedCompactJsonFormatter());
+                }
+            })
+            .CreateLogger();
+}
