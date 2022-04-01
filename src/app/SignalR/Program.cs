d5dd75b7ad90cbd47d3fb6f20b0e08cd247e37e3
Alexander Yarmolovich <yarmolovichalex@paralect.com>
2022-04-01 12:57:45 +0200

    configure logging for websocket server

--- src/app/SignalR/Program.cs
+++ src/app/SignalR/Program.cs
@@ -1,17 +1,40 @@
+using System;
+using System.Threading.Tasks;
+using Common.Utils;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace SignalR
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            using var host = CreateHostBuilder(args).Build();
+
+            var hostEnvironment = host.Services.GetRequiredService<IHostEnvironment>();
+            Log.Logger = hostEnvironment.BuildLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                await host.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
